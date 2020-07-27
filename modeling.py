19bc55669eed0d7641624d93318d230ca9bfb601
Mayner <shj030220@gmail.com>
2020-07-27 16:36:44 +0900

    Need to check about tf.variable_scope

--- modeling.py
+++ modeling.py
@@ -84,8 +84,135 @@ class BertModel(object):
         if token_type_ids is None:
             token_type_ids = tf.zeros(shape=[batch_size, seq_length], dtype=tf.int32)
 
-        with tf.variable_scope(scope, default_name="bert"):
-            with tf.variable_scope("embedding"):
+        with tf.compat.v1.variable_scope(scope, default_name="bert"):
+            with tf.compat.v1.variable_scope("embedding"):
                 (self.embedding_output, self.embedding_table) = embedding_lookup(
-                    
-                )
+                    input_ids=input_ids,
+                    vocab_size=config.vocab_size,
+                    embedding_size=config.hidden_size,
+                    initializer_range=config.initializer_range,
+                    word_embedding_name="word_embeddings",
+                    use_one_hot_embeddings=use_one_hot_embeddings)
+
+                self.embedding_output = embedding_postprocessor(
+                    input_tensor=self.embedding_output,
+                    use_token_type=True,
+                    token_type_ids=token_type_ids,
+                    token_type_vocab_size=config.type_vocab_size,
+                    token_type_embedding_name="token_type_embeddings",
+                    use_position_embeddings=True,
+                    position_embedding_name="position_embeddings",
+                    initializer_range=config.initializer_range,
+                    max_position_embeddings=config.max_position_embeddings,
+                    dropout_prob=config.hidden_dropout_prob)
+
+                with tf.compat.v1.variable_scope("encoder"):
+                    attention_mask = create_attention_mask_from_input_mask(
+                        input_ids, input_mask)
+
+                    self.all_encoder_layers = transformer_model(
+                        input_tensor=self.embedding_output,
+                        attention_mask=attention_mask,
+                        hidden_size=config.hidden_size,
+                        num_hidden_layers=config.num_hidden_layers,
+                        num_attention_heads=config.num_attention_heads,
+                        intermediate_size=config.intermediate_size,
+                        intermediate_act_fn=get_activation(config.hidden_act),
+                        hidden_dropout_prob=config.hidden_dropout_prob,
+                        attention_probs_dropout_prob=config.attention_probs_dropout_prob,
+                        initializer_range=config.initializer_range,
+                        do_return_all_layers=True)
+
+                self.sequence_output = self.all_encoder_layers[-1]
+
+                with tf.compat.v1.variable_scope("pooler"):
+                    first_token_tensor = tf.squeeze(self.sequence_output[:, 0:1, :], axis=1)
+                    self.pooled_ouput = tf.layers.dense(
+                        first_token_tensor,
+                        config.hidden_size,
+                        activation=tf.tanh,
+                        kernel_initializer=create_initializer(config.initializer_range))
+
+    def get_pooled_output(self):
+        return self.pooled_output
+
+    def get_sequence_output(self):
+        return self.sequence_output
+
+    def get_all_encoder_layers(self):
+        return self.all_encoder_layers
+
+    def get_embedding_output(self):
+        return self.embedding_output
+
+    def get_embedding_table(self):
+        return self.embedding_table
+
+def gelu(x):
+    cdf = 0.5 * (1.0 + tf.tanh(
+        (np.sqrt(2 / np.pi) * (x + 0.044715 * tf.pow(x, 3)))))
+
+    return x * cdf
+
+def get_activation(activation_string):
+    if not isinstance(activation_string, six.string_types):
+        return activation_string
+
+    if not activation_string:
+        return None
+
+    act = activation_string.lower()
+    if act == "linear":
+        return None
+    elif act == "relu":
+        return tf.nn.relu
+    elif act == "gelu":
+        return gelu
+    elif act == "tanh":
+        return tf.tanh
+    else:
+        raise ValueError("Unsupported activation: %s" % act)
+
+def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
+    assignment_map = {}
+    initialized_variable_names = {}
+
+    name_to_variable = collections.OrderedDict()
+    for var in tvars:
+        name = var.name
+        m = re.match("^(.*):\\d+$", name)
+        if m is not None:
+            name = m.group(1)
+        name_to_variable[name] = var
+
+    init_vars = tf.train.list_variables(init_checkpoint)
+
+    assignment_map = collections.OrderedDict()
+    for x in init_vars:
+        (name, var) = (x[0], x[1])
+        if name not in name_to_variable:
+            continue
+        assignment_map[name] = name
+        initialized_variable_names[name] = 1
+        initialized_variable_names[name + ":0"] = 1
+
+    return (assignment_map, initialized_variable_names)
+
+def dropout(input_tensor, dropout_prob):
+  if dropout_prob is None or dropout_prob == 0.0:
+    return input_tensor
+
+  output = tf.nn.dropout(input_tensor, 1.0 - dropout_prob)
+  return output
+
+def layer_norm(input_tensor, name=None):
+  return tf.contrib.layers.layer_norm(
+      inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
+
+def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
+  output_tensor = layer_norm(input_tensor, name)
+  output_tensor = dropout(output_tensor, dropout_prob)
+  return output_tensor
+
+def create_initializer(initializer_range=0.02):
+  return tf.truncated_normal_initializer(stddev=initializer_range)
